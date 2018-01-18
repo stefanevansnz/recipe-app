f43782ddbe49fb9771ab09345f194eecb4834c8d
Stefan Evans <stefan.evans@consegna.cloud>
2018-01-18 21:53:57 +1300

    added nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @Input() selectedHeading = 'recipes';
+
+  onNavigate(heading: string) {
+    //console.log('onNavigate = ' + heading);
+    this.selectedHeading = heading;
+  }
+
 }
