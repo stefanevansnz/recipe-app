3267592aded0591032cb8cf43c1b9cd4e391f9c4
Stefan Evans <stefan.evans@consegna.cloud>
2018-01-25 18:08:35 +1300

    adding services

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,15 @@
+import { Recipe } from "./recipe.model";
+
+export class RecipeService {
+
+    private recipes: Recipe[] = [
+        new Recipe('A Test Recipe 1', 'A simple Test 1', 'http://images.media-allrecipes.com/images/75131.jpg' ),
+        new Recipe('A Test Recipe 2', 'A simple Test 2', 'http://images.media-allrecipes.com/images/75134.jpg' )
+    ];
+    
+    getRecipes() {
+        return this.recipes.slice();
+    }
+
+
+}
