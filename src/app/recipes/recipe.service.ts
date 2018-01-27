5e36159cbf74c9ccf27b4b7a0b8a2bd0d9ad34d2
Stefan Evans <stefan.evans@consegna.cloud>
2018-01-28 09:57:44 +1300

    added recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,30 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe 1', 'A simple Test 1', 'http://images.media-allrecipes.com/images/75131.jpg' ),
-        new Recipe('A Test Recipe 2', 'A simple Test 2', 'http://images.media-allrecipes.com/images/75134.jpg' )
+        new Recipe(
+            'Schnitzel', 
+            'A yummy Schnitzel', 
+            'https://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg',
+            [
+                new Ingredient('Meat',1),
+                new Ingredient('French Fries',20)                
+            ] 
+        ),
+        new Recipe(
+            'Big Burger', 
+            'A yummy Big Burger', 
+            'https://i.ytimg.com/vi/dKjtVqLLLR0/hqdefault.jpg',
+            [
+                new Ingredient('Buns',2),
+                new Ingredient('Meat',1)              
+            ] 
+        ),
     ];
     
     getRecipes() {
