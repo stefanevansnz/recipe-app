ec8f0afc007d8e74012ddc61700e5e71cb672d62
Stefan Evans <stefan.evans@consegna.cloud>
2018-02-08 00:48:25 +1300

    added new and edit links

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { ShoppingListService } from '../../shopping-list/shopping-list.service';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -15,7 +15,8 @@ export class RecipeDetailComponent implements OnInit {
   id: number;
 
   constructor(private recipeService: RecipeService,
-              private route: ActivatedRoute) { };
+              private route: ActivatedRoute,
+              private router: Router) { };
 
   ngOnInit() {
     this.route.params
@@ -31,6 +32,10 @@ export class RecipeDetailComponent implements OnInit {
   onAddToShoppingList() {
     console.log('addToShoppingList');
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);    
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
   }
 
 }
