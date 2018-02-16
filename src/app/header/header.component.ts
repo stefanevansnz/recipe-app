77fe4507af70dce667b6609f6a957c29a0518e40
Stefan Evans <sevans@fairfaxmedia.com.au>
2018-02-17 10:33:16 +1300

    save data to backend

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
+import { Response } from '@angular/http';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,18 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataStorageService: DataStorageService) { }
 
   ngOnInit() {
+  }
+
+  onSaveData() {
+    this.dataStorageService.storeRecipes()
+      .subscribe(
+        (response: Response) => {
+          console.log(response);
+        }
+      );
   }
 
 }
---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@angular/core";
+import { Http} from "@angular/http";
+import { RecipeService } from "../recipes/recipe.service";
+
+@Injectable()
+export class DataStorageService {
+    constructor(private http: Http, private recipeService: RecipeService) {}
+
+    storeRecipes() {
+        return this.http.put('https://ng-recipe-book-3cb32.firebaseio.com/recipes.json',
+            this.recipeService.getRecipes());
+    }
+}
