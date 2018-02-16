a5ac4dd7cf99dec840daa2c2c735aa4032664e09
Stefan Evans <sevans@fairfaxmedia.com.au>
2018-02-17 11:47:39 +1300

    added get from backend

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -33,8 +33,13 @@ export class RecipeService {
         ),
     ];
 
-    constructor(private shoppingListService: ShoppingListService) {} 
-    
+    constructor(private shoppingListService: ShoppingListService) {}
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
     getRecipes() {
         return this.recipes.slice();
     }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from "@angular/core";
-import { Http} from "@angular/http";
+import { Http, Response} from "@angular/http";
 import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
+
+import 'rxjs/add/operator/map'
 
 @Injectable()
 export class DataStorageService {
@@ -9,5 +12,25 @@ export class DataStorageService {
     storeRecipes() {
         return this.http.put('https://ng-recipe-book-3cb32.firebaseio.com/recipes.json',
             this.recipeService.getRecipes());
+    }
+
+    getRecipes() {
+        this.http.get('https://ng-recipe-book-3cb32.firebaseio.com/recipes.json').map(
+                (response: Response) => {
+                    const recipes: Recipe[] = response.json();
+                    for (let recipe of recipes) {
+                        if (!recipe['ingredients']) {
+                            console.log(recipe);
+                            recipe['ingredients'] = [];
+                        }
+                    }
+                    return recipes;
+                }
+            )
+            .subscribe(
+                (recipes: Recipe[]) => {
+                    this.recipeService.setRecipes(recipes);
+                }
+            );
     }
 }
