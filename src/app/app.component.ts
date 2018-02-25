b4f10182b8a427642330c291fb192194665c4f23
Stefan Evans <sevans@fairfaxmedia.com.au>
2018-02-25 17:03:15 +1300

    connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component, Input } from '@angular/core';
-
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   @Input() selectedHeading = 'recipes';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: //"",
+      authDomain: "ng-recipe-book-3cb32.firebaseapp.com"
+    });
+  }
 
   onNavigate(heading: string) {
     //console.log('onNavigate = ' + heading);
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms/src/directives/ng_form';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -8,7 +9,7 @@ import { NgForm } from '@angular/forms/src/directives/ng_form';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
@@ -16,8 +17,9 @@ export class SignupComponent implements OnInit {
   onSignup(form: NgForm) {
     const email = form.value.email;
     const password = form.value.password;
+    this.authService.signupUser(email, password);
 
-    
+
   }
 
 }
